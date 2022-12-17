a2810a611c2e677a436041dd65d895d1880fadf3
ROVEN KYLE <ellamateogang@gmail.com>
2022-12-18 01:50:26 +0800

    added user base navigation

--- Screens/TCScreen.js
+++ Screens/TCScreen.js
@@ -3,28 +3,41 @@ import React from 'react'
 import { ProceedButton } from '../ScreenComponents/Buttons'
 import { useNavigation } from '@react-navigation/native'
 import { FlipLogo } from '../ScreenComponents/FlipLogo'
-export default function TCScreen() {
+import { useSelector } from 'react-redux';
 
-  const navigation = useNavigation()
 
-  return (
-    <View style = {{justifyContent: 'center', alignItems: 'center', flex: 1}} >
-      <FlipLogo/>
-      <Text>You must use this system with accordance to the privacy policy of Pangasinan State University</Text>
-      <View style = {{flexDirection: 'row', margin: 20}} >
-      <ProceedButton
-      title = "Proceed"
-      onPress = {()=>navigation.navigate('GuestLoginScreen')}
-      style = {{backgroundColor: '#fff', margin: 5, borderRadius: 5,}}
-      titleStyle = {{color: '#000'}}
-      />
-      <ProceedButton
-      title = "Exit"
-      onPress = {()=> navigation.navigate('InitialRoutingScreen')}
-      style = {{backgroundColor: '#fff', margin: 5, borderRadius: 5,}}
-      titleStyle = {{color: '#000'}}
-      />
-      </View>
+  export default function TCScreen() {
+
+    const user = useSelector(state => state.essensials.user)
+
+    const navigation = useNavigation()
+
+    const proceed = () => {
+      if(user == 'STUDENT'){
+        navigation.navigate('StudentLoginScreen')
+      }else{
+        navigation.navigate('GuestLoginScreen')
+      }
+    }
+
+    return (
+      <View style = {{justifyContent: 'center', alignItems: 'center', flex: 1}} >
+        <FlipLogo/>
+          <Text>You must use this system with accordance to the privacy policy of Pangasinan State University</Text>
+            <View style = {{flexDirection: 'row', margin: 20}} >
+              <ProceedButton
+                title = "Proceed"
+                onPress = {proceed}
+                style = {{backgroundColor: '#fff', margin: 5, borderRadius: 5,}}
+                titleStyle = {{color: '#000'}}
+              />
+              <ProceedButton
+                title = "Exit"
+                onPress = {()=> navigation.navigate('InitialRoutingScreen')}
+                style = {{backgroundColor: '#fff', margin: 5, borderRadius: 5,}}
+                titleStyle = {{color: '#000'}}
+              />
+            </View>
     </View>
   )
 }
