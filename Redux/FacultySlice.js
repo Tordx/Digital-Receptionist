14fe8464bb803954b45c913bf84f9ef5fb2ea601
Miklos <miklos.herald@gmail.com>
2023-02-11 02:49:00 +0800

    Change values

--- Redux/FacultySlice.js
+++ Redux/FacultySlice.js
@@ -4,18 +4,12 @@
  export const FacultySlice = createSlice({
    name: 'facultymodal',
    initialState: {
-    isOpen: false,
-    facultyData: []
+    facultyDatas: []
    },
    reducers: {
-      openFacultyModal: (state , action) => {
-        state.isOpen = true
-      },
-      closeFacultyModal: (state , action) => {
-        state.isOpen = false
-      },
-      setFacultyData: (state , action ) => {
-        state.facultyData = action.payload
+
+      setFacultyDatas: (state , action ) => {
+        state.facultyDatas = action.payload
         console.log(action)
         console.log('action')
       },
@@ -24,6 +18,6 @@
  })
  
  // Action creators are generated for each case reducer function
- export const {openFacultyModal , closeFacultyModal , setFacultyData} = FacultySlice.actions
+ export const {setFacultyDatas} = FacultySlice.actions
  
  export default FacultySlice.reducer
