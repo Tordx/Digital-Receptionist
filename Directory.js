570acfc26d6aa1c6b3a163d31696d1a3dfd38aa5
Miklos <miklos.herald@gmail.com>
2023-02-11 02:43:21 +0800

    added maps

--- Directory.js
+++ Directory.js
@@ -5,36 +5,34 @@ import { Provider } from 'react-redux';
 import store from './Redux/Store';
 
 import InitialRoutingScreen from './Screens/InitialRoutingScreen';
-import IdleScreen from './Screens/IdleScreen';
 import ReportScreen from './Screens/ReportScreen';
 import TCScreen from './Screens/TCScreen';
 // import UniversityMap from './Screens/UniversityMap';
 //Student StackScreens
-import Employee from './Navigations/Employee';
 import ClassScreen from './Screens/Navigations/Student/ClassScreen';
 import AdminScreen from './Screens/Navigations/Student/AdminScreen';
 import FacultySreen from './Screens/Navigations/Student/FacultyScreen';
 import EventScreen from './Screens/Navigations/Student/EventScreen';
 import OfficesScreen from './Screens/Navigations/Student/BuildingScreen';
 import SuggestionsScreen from './Screens/SuggestionsScreen';
-import AddClassScreen from './Screens/Navigations/Employee/AddClassScreen';
+import AddClassScreen from './Screens/Navigations/Admin/AddClassScreen';
 import StudentLoginScreen from './Screens/Navigations/Student/StudentLoginScreen';
 import StudentHomeScreen from './Screens/Navigations/Student/StudentHomeScreen';
 // Admin StackScreen
-import AdminHomeScreen from './Screens/Navigations/Employee/AdminHomeScreen';
-import AdminMainMenu from './Screens/Navigations/Employee/AdminMainMenu';
-import AdminLoginScreen from './Screens/Navigations/Employee/AdminLoginScreen';
-import FacultyConfig from './Screens/Navigations/Employee/FacultyConfig';
-import AddFacultyScreen from './Screens/Navigations/Employee/AddFacultyScreen';
-import AddAdminScreen from './Screens/Navigations/Employee/AddAdminScreen';
-import AddEventScreen from './Screens/Navigations/Employee/AddEventScreen';
-import AdminReports from './Screens/Navigations/Employee/AdminReports';
-import AddStudentScreen from './Screens/Navigations/Employee/AddStudentScreen';
-import AddSuperAdmin from './Screens/Navigations/Employee/AddSuperAdmin';
-import LogBookScreen from './Screens/Navigations/Employee/LogBookScreen';
-import AddBuildingScreen from './Screens/Navigations/Employee/AddBuildingScreen';
+import AdminHomeScreen from './Screens/Navigations/Admin/AdminHomeScreen';
+import AdminMainMenu from './Screens/Navigations/Admin/AdminMainMenu';
+import AdminLoginScreen from './Screens/Navigations/Admin/AdminLoginScreen';
+import AddFacultyScreen from './Screens/Navigations/Admin/AddFacultyScreen';
+import AddAdminScreen from './Screens/Navigations/Admin/AddAdminScreen';
+import AddEventScreen from './Screens/Navigations/Admin/AddEventScreen';
+import AdminReports from './Screens/Navigations/Admin/AdminReports';
+import AddStudentScreen from './Screens/Navigations/Admin/AddStudentScreen';
+import AddSuperAdmin from './Screens/Navigations/Admin/AddSuperAdmin';
+import LogBookScreen from './Screens/Navigations/Admin/LogBookScreen';
+import AddBuildingScreen from './Screens/Navigations/Admin/AddBuildingScreen';
 // Guest StackScreen
-import Camera from './ScreenComponents/Camera';
+import Camera from './Components/Camera';
+import Maps from './Screens/Maps';
 
 export default function Directory() {
   
@@ -45,13 +43,11 @@ export default function Directory() {
         <NavigationContainer>
             <Stack.Navigator initialRouteName='IntialRoutingScreen'>
 
-                
+               
                 <Stack.Screen name = 'InitialRoutingScreen' component={InitialRoutingScreen} options = {{ headerShown: false }}/>
-                {/* <Stack.Screen name = 'UniversityMap' component={UniversityMap} options = {{headerShown: false}}/> */}
+                <Stack.Screen name = 'Maps' component={Maps} options = {{headerShown: false }}/>
                 <Stack.Screen name = 'StudentHomeScreen' component={StudentHomeScreen} options = {{ headerShown: false }} />
-                <Stack.Screen name = 'Employee' component = {Employee} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'ReportScreen' component={ReportScreen} options = {{ headerShown: false }} />
-                <Stack.Screen name = 'IdleScreen' component={IdleScreen} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'StudentLoginScreen' component={StudentLoginScreen} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'ClassScreen' component={ClassScreen} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'AdminScreen' component={AdminScreen} options = {{ headerShown: false }} />
@@ -62,7 +58,6 @@ export default function Directory() {
                 <Stack.Screen name = 'AdminHomeScreen' component={AdminHomeScreen} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'AdminMainMenu' component={AdminMainMenu} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'AddClassScreen' component={AddClassScreen} options = {{ headerShown: false }} />
-                <Stack.Screen name = 'FacultyConfig' component={FacultyConfig} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'AdminLoginScreen' component={AdminLoginScreen} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'AddFacultyScreen' component={AddFacultyScreen} options = {{ headerShown: false }} />
                 <Stack.Screen name = 'AddAdminScreen' component={AddAdminScreen} options = {{ headerShown: false }} />
