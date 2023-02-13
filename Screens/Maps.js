2263512fe42b825d2a564a989a16cc2949212a1f
Roven Kyle Narra <ellamateogang@gmail.com>
2023-02-13 21:12:03 +0800

    will be remove as (unused)

--- Screens/Maps.js
+++ Screens/Maps.js
@@ -16,42 +16,13 @@ export default function Maps() {
   const navigation = useNavigation();
   const [buildingCoordinates, setBuildingCoordinates] = useState('')
   const {facultyDatas} = useSelector((store) => store.facultymodal)      
-  const [center, setCenter] = useState([120.22978878878246, 16.032108026014853])                                                                 
+  const [center, setCenter] = useState([120.2307078878246, 16.032108026014853])                                                                 
   const [AccessToken] = useState(facultyDatas.Token)
   MapboxGL.setWellKnownTileServer('Mapbox'); // error making mapbox v-11 style must include this component
   MapboxGL.setAccessToken(AccessToken)
 
-  useEffect(() => {
-    BuildingCoordinates();
-  },[])
-
-  const BuildingCoordinates = async() => {
-
-    var result =  await remoteDBBuilding.allDocs({
-        include_docs: true,
-        attachments: true,
-    })
-    if(result.rows){
-      let modifiedArr = result.rows.map(function(item) {
-        return item.doc
-      })
-      let filteredData = modifiedArr.filter(item => {
-        return item
-      })
-      if(filteredData) {
-        let newFilterData = filteredData.map(item => {
-          return item;
-        });
-        setBuildingCoordinates(newFilterData);
-        console.log(newFilterData)
-
-      }
-    } 
-
-  }
-
   return (
-    <View style = {{width: '50%', height: '95%', borderRadius: 5, marginLeft: 20}}>
+    <View style = {{width: '100%', height: '100%', borderRadius: 5, marginLeft: 20, position: 'absolute'}}>
     <MapboxGL.MapView
    style = {{flex: 1, borderRadius: 5}}
    styleURL = 'mapbox://styles/kalokalo/cldzyog2k000a01t401qifylc'
@@ -60,8 +31,8 @@ export default function Maps() {
    attributionPosition = {{bottom: 10, left: 10}}
    >
     <MapboxGL.Camera
-      centerCoordinate={facultyDatas.Coordinates}
-      zoomLevel = {17.9}
+      centerCoordinate={center}
+      zoomLevel = {17.8}
     />
       <MapboxGL.PointAnnotation
         id = {facultyDatas.CollegeAcronym}
@@ -72,5 +43,3 @@ export default function Maps() {
    </View>
   );
 }
-
-// Need to setup API maps at mapbox, already have an account needs to access lol
