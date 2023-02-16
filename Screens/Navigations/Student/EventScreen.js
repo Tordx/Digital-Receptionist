00ca704c94bb7bc7da8b39357e592fa5dedcafb6
Tordx <ellamateogang@gmail.com>
2023-02-17 01:09:56 +0800

    added modal for image clicks

--- Screens/Navigations/Student/EventScreen.js
+++ Screens/Navigations/Student/EventScreen.js
@@ -1,54 +1,61 @@
-import { View, Text, StyleSheet, FlatList, SafeAreaView , TouchableOpacity , Image, ImageBackground, Platform } from 'react-native'
+import { View, Text, StyleSheet, FlatList, SafeAreaView , TouchableOpacity , Image, ImageBackground, Platform, Pressable, Modal, StatusBar } from 'react-native'
 import React , {useState , useEffect} from 'react';
 import { CloseButton } from '../../../Components/Buttons';
 import { useNavigation } from '@react-navigation/native';
-import { remoteDBEvent } from '../../../Database/pouchDb';
 import EventModal from '../../../Modal/EventModal';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { openEventModal } from '../../../Redux/EventSlice';
 import { setEventData } from '../../../Redux/EventSlice';
-import { ImageSlider } from 'react-native-image-slider-banner';
-import Slideshow from 'react-native-slideshow-improved';
+import { remoteDBEvent } from '../../../Database/pouchDb';
 export default function EventScreen() {
 
     useEffect(() => {
-      ImageData()
-    }, []);
+      EventData()
+    }, [event]);
 
-  const [imagedata, setImageData] = useState('');
-  const ImageData  = () => {
-   const image  = [
-      {
-        img: "https://satriyaadikaums.files.wordpress.com/2019/10/welcome-slider-1024x446.jpg?w=1024]",
-        _id: '2211'
-      },
-      {
-        img: "https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",
-        _id: '223'
-      },
-      {
-        img: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__340.jpg",
-        _id: '332'
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
+  const [event, setEvent] = useState();
+  const [openModal, setOpenModal] = useState(false);
+  const {eventData} = useSelector((store) => (store.eventmodal))
+  StatusBar.setHidden(true);
+  const EventData = async() => {
+
+    var result = await remoteDBEvent.allDocs({
+      include_docs: true,
+      attachments: true,
+    });
+    if(result.rows) {
+      let modifiedArr =  result.rows.map(function(item) {
+        return item.doc
+      });
+      let filteredData = modifiedArr.filter(item => {
+        return item
+      })
+      if(filteredData){
+       let newFilterData = filteredData.map((item) => {
+        return item 
+       })
+       setEvent(newFilterData);
+       console.log(newFilterData)
       }
-  ]
-  setImageData(image)
-}
+    }
 
-    const navigation = useNavigation();
+  }
 
-    const [reference, setReference] = useState(null)
 
     const renderItem = ({item}) => {
 
       return (
-        <View styler = {{justifyContent: 'center', alignContent: 'center', alignSelf: 'center',}}>
+        <Pressable style = {{justifyContent: 'center', alignContent: 'center', alignSelf: 'center',}}
+          onPress = {() => { dispatch(setEventData(item)); setOpenModal(true)}}
+        >
           <Image
-            source = {{uri: item.img}}
+            source = {{uri: item.EventImage}}
             style = {{width: 1280, height: '100%', alignSelf: 'center'}}
             resizeMode = 'contain'
           />
-        </View>
+        </Pressable>
       )
     }
     
@@ -56,7 +63,7 @@ export default function EventScreen() {
       <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000'}}>
        
         <FlatList
-          data = {imagedata}
+          data = {event}
           horizontal
           renderItem = {renderItem}
           keyExtractor = {item => item._id}
@@ -68,6 +75,30 @@ export default function EventScreen() {
         color = {'#fff'}
         onPress = {() => navigation.goBack('StudentHomeScreen')}
         />
+        <Modal
+          statusBarTranslucent 
+          visible = {openModal}
+          onRequestClose = {() => setOpenModal(false)}
+          animationType = 'fade'
+          transparent
+        >
+         <Pressable style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} onPress = {() => setOpenModal(false)}>
+         <StatusBar barStyle= 'light-content'/>
+           {/* <Image
+            source = {{uri: eventData.EventImage}}
+            resizeMode = 'contain'
+            style = {{width: '100%', height: 800}}
+           /> */}
+           <View style = {{width: '100%', height: 150, backgroundColor: '#00000070', position: 'absolute', bottom: 0, justifyContent: 'center', }}>
+            <View style = {{padding: 20, paddingLeft: 50, justifyContent: 'flex-start', height: '100%'}}>
+            <Text style = {{color: '#fff', fontSize: 25, fontWeight: '500'}}>{eventData.EventName}</Text>
+            <Text style = {{color: '#fff', fontSize: 17}}>{eventData.EventTagline}</Text>
+            <Text style = {{color: '#fff', fontSize: 17}}>{eventData.EventWhere}</Text>
+            <Text style = {{color: '#fff', fontSize: 17}}>{eventData.EventWhen}</Text>
+            </View>
+           </View>
+        </Pressable>
+        </Modal>
       </View>
     )
 
