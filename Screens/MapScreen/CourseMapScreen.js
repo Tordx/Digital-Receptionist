aafec36f12d347b3393a83a84cf600f0350d08be
Tordx <ellamateogang@gmail.com>
2023-02-20 01:50:43 +0800

    logo pulling

--- Screens/MapScreen/CourseMapScreen.js
+++ Screens/MapScreen/CourseMapScreen.js
@@ -4,21 +4,47 @@ import React, { Component, useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar, FlatList, RefreshControl, Image } from 'react-native';
 import { useSelector } from 'react-redux';
 import { CloseButton } from '../../Components/Buttons';
-import { remoteDBfacultyMember } from '../../Database/pouchDb';
+import { remoteDBfacultyMember, remoteDBOrg } from '../../Database/pouchDb';
 import Maps from '../../Components/Maps';
 
 export default function CourseMapScreen ()  {
 
     useEffect(() => {
-        MemberDetails();
+      OrgList();
+      MemberList()
       },[])
 
     const {courseData} = useSelector((store) => (store.classmodal))
     const navigation = useNavigation()
-    const [memberdetails, setMemberFaculty] = useState([]);
-    const [memberRefresh, setMemberRefresh] = useState('');
+    const [orgdetail, setOrgDetail] = useState([]);
+    const [memberdetails, setMemberDetail] = useState();
+    const [memberRefresh, setMemberRefresh] = useState(false);
     
-      const MemberDetails = async() => {
+      const OrgList = async() => {
+    
+        var result =  await remoteDBOrg.allDocs({
+            include_docs: true,
+            attachments: true,
+        })
+        if(result.rows){
+          let modifiedArr = result.rows.map(function(item) {
+            return item.doc
+          })
+          let filteredData = modifiedArr.filter(item => {
+            return item.CollegeAcronym === courseData.CollegeAcronym
+          })
+          if(filteredData) {
+            let newFilterData = filteredData.map(item => {
+              return item;
+            });
+            setOrgDetail(newFilterData);
+            console.log(newFilterData)
+    
+          }
+        } 
+    
+      }
+      const MemberList = async() => {
     
         var result =  await remoteDBfacultyMember.allDocs({
             include_docs: true,
@@ -29,13 +55,14 @@ export default function CourseMapScreen ()  {
             return item.doc
           })
           let filteredData = modifiedArr.filter(item => {
-            return item.CollegeAcronym  === courseData.CollegeAcronym
+            return item.CollegeAcronym === courseData.CollegeAcronym
           })
           if(filteredData) {
             let newFilterData = filteredData.map(item => {
               return item;
             });
-            setMemberFaculty(newFilterData);
+            setMemberDetail(newFilterData);
+            console.log(newFilterData)
     
           }
         } 
@@ -44,21 +71,38 @@ export default function CourseMapScreen ()  {
       const RefreshList = () => {
 
         setMemberRefresh(true);
-        MemberDetails();
+        OrgList();
+        MemberList();
         setMemberRefresh(false);
   
     }
 
     const renderItem = ({item}) => {
 
-        return (
-        <View style = {{flexDirection: 'row'}}>
-            <Text>{item.Name} — </Text>
-            <Text>{item.Title}</Text>
+      return (
+        <View style = {{flexDibrection: 'column'}}>
+        <View style = {{flexDirection: 'column', alignItems: 'flex-start',}}>
+            <Text style = {{fontSize: 19, color: '#505050' }}>{item.Title}  —  {item.Name}</Text>
+            
         </View>
-        )
+        </View>
+      )
 
-    }
+    } 
+    const orgItem = ({item}) => {
+
+      return (
+      <View style = {{flexDirection: 'row'}}>
+          <Image
+            source = {{uri: item.Image}}
+            style = {{width: 75, height: 75}}
+            resizeMode = 'contain'
+          />
+      </View>
+      )
+
+  }
+
 
     return (
         <>
@@ -68,62 +112,55 @@ export default function CourseMapScreen ()  {
             />
           <Maps
           id = {courseData.CourseAcronym}
-          title = {courseData.College}
+          title = {courseData.Course}
           coordinate = {courseData.Coordinates}
           />
           <View style = {{width: '50%', height: '100%', justifyContent: 'center', alignItems: 'center', position: 'absolute', right: 0}} >
+            
+         
             <View style = {{position: 'absolute', top: 10, left: 20, width: '100%', height: '100%',}}>
               <View style = {{justifyContent: 'center',   backgroundColor: '#fff', padding: 30, borderTopLeftRadius: 20, borderBottomLeftRadius: 20, elevation: 10,}}>
-                <Text style = {{fontSize: 25, fontWeight: '500' }}>
+              <RefreshControl
+                  refreshing = {memberRefresh}
+                  onRefresh = {RefreshList}
+                  style = {{backgroundColor: 'green'}}
+              />
+                <Text style = {{fontSize: 25, fontWeight: '500', color: '#505050'  }}>
                   {courseData.College}
                 </Text>
-                <Text style = {{fontSize: 20, fontWeight: '300' }}>
-                {courseData.Dean} — College Dean
-              </Text>
-              <Text style = {{fontSize: 20, fontWeight: '300' }}>
-                {courseData.ChairPerson} — Chair Person
+              <Text style = {{fontSize: 20, fontWeight: '300', color: '#505050'  }}>
+              <Text style = {{fontSize: 20, fontWeight: '500', color: '#505050'  }}>Chairperson </Text>— {courseData.ChairPerson} 
               </Text>
             </View>
             <View style = {{ marginTop: 5,justifyContent: 'center',   backgroundColor: '#fff', padding: 30, borderTopLeftRadius: 20, borderBottomLeftRadius: 20, elevation: 10}}>
                 
-                <Text style = {{fontSize: 20, fontWeight: '300' }}>
+                <Text style = {{fontSize: 20, fontWeight: '300', color: '#505050'  }}>
                 {courseData.Building}
               </Text>
             </View>
             <View style = {{ marginTop: 5,justifyContent: 'center',   backgroundColor: '#fff', padding: 30, borderTopLeftRadius: 20, borderBottomLeftRadius: 20, elevation: 10}}>
                 
-                <Text style = {{fontSize: 16, fontWeight: '300' }}>
+                <Text style = {{fontSize: 16, fontWeight: '300', marginBottom: 10, color: '#505050'  }}>
                 Organization/s under {courseData.Course}
               </Text>
-              {/* <FlatList
-              data={orgdetails}
-              renderItem={imageRender}
+              <FlatList
+              
+              horizontal
+              data={orgdetail}
+              renderItem={orgItem}
               keyExtractor={(item) => item._id}
-              refreshControl = {
-                <RefreshControl
-                  refreshing = {memberRefresh}
-                  onRefresh = {RefreshList}
-                  style = {{backgroundColor: 'green'}}
-                />
-              }
               
             />
-              */}
+             
             </View>
             <View style = {{ marginTop: 5,justifyContent: 'center',   backgroundColor: '#fff', padding: 30, borderTopLeftRadius: 20, borderBottomLeftRadius: 20, elevation: 10}}>
+            <Text style = {{fontSize: 16, fontWeight: '300', marginBottom: 10, color: '#505050'  }}>
+                Teaching Faculty
+              </Text>
             <FlatList
               data={memberdetails}
-              horizontal
               renderItem={renderItem}
               keyExtractor={(item) => item._id}
-              refreshControl = {
-                <RefreshControl
-                  refreshing = {memberRefresh}
-                  onRefresh = {RefreshList}
-                  style = {{backgroundColor: 'green'}}
-                />
-              }
-              
             />
             </View>
           </View>
