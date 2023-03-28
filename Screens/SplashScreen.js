491c413c45a6bf6570cb9e439529462493c19ccb
Roven Kyle Narra <ellamateogang@gmail.com>
2023-03-28 22:41:37 +0800

    splashscreen done

--- Screens/SplashScreen.js
+++ Screens/SplashScreen.js
@@ -1,30 +1,23 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { Component } from 'react';
 import { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const SplashScreen = () => {
+
+    const navigation = useNavigation()
 
     useEffect(() => {
         
         setTimeout(() => {
             navigation.replace('CitizenChartScreen');
-        }, 1000);
+        }, 3000);
     })
 
     return (
-        <View style={styles.container}>
-            <Text>MyComponent</Text>
-        </View>
+        <>
+        <Image style = {{width: '101%', height: '100%', justifyContent: 'center', alignSelf: 'center'}} resizeMode = 'contain' source={require('../Assets/Img/splash.png')} />
+        </>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
-});
-
 export default SplashScreen;
