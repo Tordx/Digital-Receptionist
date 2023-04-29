c126758c033d03d92c8cea34df9abdbff42fe38a
Roven Kyle Narra <ellamateogang@gmail.com>
2023-04-29 22:04:26 +0800

    building fixed for better view

--- Screens/Navigations/Student/BuildingScreen.js
+++ Screens/Navigations/Student/BuildingScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, FlatList, SafeAreaView , TouchableOpacity , Image, ImageBackground, Platform, Pressable, Modal, StatusBar } from 'react-native'
+import { View, Text, StyleSheet, FlatList, TextInput , TouchableOpacity , Image, ImageBackground, Platform, Pressable, Modal, StatusBar, ScrollView, RefreshControl } from 'react-native'
 import React , {useState , useEffect ,useCallback} from 'react';
 import { CloseButton } from '../../../Components/Buttons';
 import { useNavigation } from '@react-navigation/native';
@@ -13,23 +13,16 @@ export default function EventScreen() {
 
     useEffect(() => {
       EventData()
-    }, [event]);
+    }, [searchTerm]);
 
   const dispatch = useDispatch()
   const navigation = useNavigation();
   const [event, setEvent] = useState();
-  const [data, setData] = useState("");
+  const [data, setData] = useState([]);
+  const [showSearch, setShowSearch] = useState(false)
+  const [searchTerm, setSearchTerm] = useState([])
+  const [courseRefresh, setCourseRefresh] = useState(false);
   StatusBar.setHidden(true);
-
-  const handleViewableItemsChanged = useCallback(
-    ({ viewableItems, changed }) => {
-      setData(viewableItems[0].item)
-      console.log('====================================viewableItems[0].item');
-      console.log(viewableItems[0].item);
-      console.log('====================================viewableItems[0].item');
-    },
-    []
-  );
 
 
   
@@ -44,13 +37,15 @@ export default function EventScreen() {
         return item.doc
       });
       let filteredData = modifiedArr.filter(item => {
-        return item
+        return item && 
+        new RegExp(searchTerm, 'i').test(item.BuildingName) ||
+        new RegExp(searchTerm, 'i').test(item.BuildingLocation)
       })
       if(filteredData){
        let newFilterData = filteredData.map((item) => {
         return item 
        })
-       setEvent(newFilterData);
+       setSearchTerm(newFilterData);
        console.log('newFilterData')
        console.log(newFilterData)
        console.log('newFilterData')
@@ -59,54 +54,95 @@ export default function EventScreen() {
 
   }
 
-    const renderItem = ({item}) => {
+  const RefreshList = () => {
 
-      return (
-      <Pressable onPress={() => {
-        dispatch(setBuildingData(item)); navigation.navigate('BuildingMapScreen')
-     }}>
-          <Image
-            source = {{uri: item.BuildingPicture}}
-            style = {{width: 1300, height: '100%', alignSelf: 'center', backgroundColor: 'black'} }
-            resizeMode = 'contain'
-          />
-       </Pressable>
-      )
-    }
+    setCourseRefresh(true);
+    renderCourse();
+    setCourseRefresh(false);
+
+}
+
+
+
+  const renderItem = ({ item }) => {
+
+    return(
+      <Pressable 
+      style = {styles.item}
+      android_ripple={{
+        color: 'blue',
+        borderRadius: 100,
+        radius: 200,
+      }} 
+      onPress={() => {
+         dispatch(setAdminData(item))
+      }} >
+        <View style = {{marginLeft: 10, justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
+        <Image  resizeMode='cover' style = {{width: 175, height: 230, borderRadius: 30}} source = {{uri:  item.BuildingPicture}}/>
+        <View style = {{width: '50%', height: '98%', alignItems: 'flex-start', marginLeft: 10}}>
+          <Text style ={{fontSize: 20, top: 0, fontFamily: 'black', color: '#404040'}} >{item.BuildingName} </Text>
+          <Text style ={{fontSize: 17, top: 0, fontFamily: 'regular', color: '#404040'}} >{item.Rooms ? `${item.Rooms.length} room(s)` : 'No rooms'}</Text>
+        </View>
+        </View> 
+    </Pressable>
+    )
+  }
 
     return (
       <View style={styles.container}>
-
-        <View>
+        <ScrollView showsVerticalScrollIndicator = {false}>
         <FlatList
-          data = {event}
-          horizontal
-          renderItem = {renderItem}
-          keyExtractor = {item => item._id}
-          onViewableItemsChanged={handleViewableItemsChanged}
-          pagingEnabled
-        />
-      </View>
+          style = {{paddingTop: 100}}
+          data={searchTerm}
+          numColumns = {3}
+          renderItem={renderItem}
+          keyExtractor={(item) => item._id}
+          showsVerticalScrollIndicator = {false}
+          refreshControl = {
+            <RefreshControl
+              refreshing = {courseRefresh}
+              onRefresh = {RefreshList}
+            />
+            
+          }
+          />
+          </ScrollView>
         <CloseButton style = {{position: 'absolute', top: 10, left: 10}}
         name = 'arrow-back'
         size = {35}
-        color = {'#fff'}
+        color = {'#404040'}
         onPress = {() => navigation.goBack('StudentHomeScreen')}
         />
-        
-         <StatusBar barStyle= 'light-content'/>
-           <View style = {{width: '100%', height: '20%', backgroundColor: 'black' }}>
-            <View style = {{justifyContent: 'flex-start', height: '100%'}}>
-            <Text style = {{color: '#fff', fontSize: 35, fontWeight: '500'}}>Building Name:{data.BuildingName}</Text>
-            <Text style = {{color: '#fff', fontSize: 25}}>Building Location: {data.BuildingLocation}</Text>
-            {/* <Text style = {{color: '#fff', fontSize: 25}}>Event Where: {data.EventWhere}</Text>
-            <Text style = {{color: '#fff', fontSize: 25}}>Event When: {data.EventWhen}</Text>
-            <Text style = {{color: '#fff', fontSize: 25}}>Event Description: {data.EventDescription}</Text> */}
-            </View>
+           <View style = {{backgroundColor: '#0f2ed6', padding: 10, borderRadius: 20, position: 'absolute', top: 20, elevation: 10, shadowColor: '#000'}}>
+          {showSearch ? null : <Text style = {{fontSize: 30, fontWeight: 'bold', color: '#fff'}}>CAMPUS BUILDINGS</Text>}
+          </View>
+        {showSearch ?
+          <View style = {styles.TextInput}>
+          <TextInput
+              style  = {{width: '100%', fontSize: 17}}
+              value={searchTerm} 
+              onChange={(event) => {
+                setSearchTerm(event.nativeEvent.text) }}
+              placeholder = 'Search Citizen Charter...'
+            
+          />
+              <CloseButton
+            style={styles.searchButtonExit}
+            name='close'
+            size={35}
+            color={'black'}
+            onPress={() => setShowSearch(!showSearch)}
+          />
+          
+          </View>
+       :  <CloseButton
+       style={styles.searchButton}
+       name='search'
+       size={35}
+       color={'#0f2ed6'}
+       onPress={() => setShowSearch(!showSearch)}
+     />}  
            </View>
-     
-
-      </View>
     )
 
 };
@@ -115,94 +151,91 @@ export default function EventScreen() {
 
  const styles = StyleSheet.create({
           
-        eventcontainer: {
-          
-          alignItems: 'center', 
-          justifyContent: 'center', 
-          width: '50%'
-        
-        },
-      
-        imagecontainer: {
-          
-          borderRadius: 20,
-          width: '100%', 
-          height: '100%', 
-          justifyContent: 'center', 
-          alignItems: 'center',
-        
-        },
-      
-        inputcontainer:{
-          
-          justifyContent: 'center', 
-          alignItems: 'center', 
-          width: '100%',
-          height: '100%',
-        
-        },
-      
-        contentcontainer: {
-          
-          justifyContent: 'center', 
-          alignItems: 'center', 
-          width: '100%',
-          height: '100%',
-          flexDirection: 'row'
-        
-        },
-      
-        TextInput: {
-      
-          backgroundColor: '#f2f3f7',
-          width: '80%',
-          borderRadius: 5,
-          height: 50,
-          marginTop: 10,
-          alignItems: 'center',
-          flexDirection: 'row',
-          shadowColor: "#000",
-          shadowOffset: {
-            width: 1,
-            height: 2,
-          },
-          shadowOpacity: 0.1,
-          shadowRadius: 1,
-          elevation: 3,
-      
-        },
-      
-        nextbutton: {
-          
-          backgroundColor: '#0f2ed6',
-          width: '100%',
-          height: 75,
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRightWidth: 3,
-          bottom: 0,
-          position: 'absolute'
-      
-        },
-      
-        imagebutton: {
-      
-          width: '50%',
-          height: '50%',
-          justifyContent: 'center',
-          alignItems: 'center',
-          position: 'absolute',
-          borderRadius: 10,
-      
-        },
-      
-        container: {
-      
-            width: '100%',
-            height: '100%',
-            justifyContent: 'center',
-            alignItems: 'center',
-            backgroundColor: '#f2f3f7',
-        },
-          
-      })
+  container: {
+
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f2f3f7',
+    
+},
+
+contentcontainer: {
+
+  width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+},
+
+item: {
+
+  alignItems: 'center',
+  justifyContent: 'flex-start',
+  backgroundColor: '#FFFF',
+  borderWidth: 1,
+  borderColor: '#0f2ed6',
+  width: 400,
+  height: 250,
+  borderRadius: 30,
+  marginVertical: 5,
+  marginHorizontal: 5,
+  elevation: 1,
+  flexDirection: 'row'
+
+},
+
+title: {
+
+  fontSize: 16,
+  textAlign: 'center',
+  color: '#404040',
+  width: '85%',
+  fontFamily: 'regular'
+
+
+},
+
+text: {
+
+  fontSize: 25,
+  left: 0,
+  textAlign: 'center',
+  color: 'white',
+  fontFamily: 'regular'
+
+},
+
+TextInput: { 
+
+  position: 'absolute', 
+  top: 20, 
+  alignSelf:'center', 
+  flexDirection: 'row',
+  backgroundColor: '#ffff',
+  width: 600,
+  borderRadius: 4,
+  height: 50,
+  elevation: 1,
+  borderWidth: .5,
+  borderColor: '#a2a2a2'
+
+},
+closeButton: {
+  position: 'absolute',
+  top: 20,
+  left: 20,
+},
+searchButton: {
+  position: 'absolute',
+  top: 20,
+  right: 20,
+},
+searchButtonExit: {
+  position: 'absolute',
+  top: 5,
+  right: 10,
+},
+
+});
