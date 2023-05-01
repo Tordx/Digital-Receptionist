0683fcaa5a7e91ef972baf3f1e22488b169ddb2e
Tordx <ellamateogang@gmail.com>
2023-05-02 02:47:05 +0800

    reports done

--- Screens/ReportScreen.js
+++ Screens/ReportScreen.js
@@ -53,25 +53,26 @@ export default function SuggestionsScreen() {
 
   return (
     <View  style = {styles.Container}>
-      <CloseButton
+        <CloseButton
       
-        name = "arrow-back"
-        onPress = {() => navigation.navigate('InitialRoutingScreen')}
-        size = {50}
-        color = 'white'
-        style = {{flexDirection: 'row', top: 0, left: 0, position: 'absolute', marginVertical: 27, marginHorizontal: 20}}
+      name = "arrow-back"
+      onPress = {() => navigation.navigate('StudentHomeScreen')}
+      size = {35}
+      color = {'white'}
+      style = {{flexDirection: 'row', top: 0, left: 0, position: 'absolute', marginVertical: 27, marginHorizontal: 20}}
 
-      />
+    />
     <View style = {styles.BoxContainer}>
-      <View style = {{flexDirection: 'row',}}>
-        <View style = {styles.ButtonContainer} >
-          <RadioButton
+    <View style = {{justifyContent: 'center', alignItems: 'center', width: '50%', height: '100%', padding: 10  , }}>
+    <View style = {{flexDirection: 'row',}}>
+      <View style = {styles.ButtonContainer} >
+      <RadioButton
             value="Report"
             color='#eb4034'
             status={checked === 'Report' ? 'checked' : 'unchecked' }
             onPress={() => setChecked('Report')}
           />
-          <Text style = {{fontSize: 20,}}>Complaint</Text>
+            <Text style = {{fontSize: 20,fontFamily: 'extrabold', color: '#202020',textShadowRadius: 5, textShadowColor: '#fff'}}>Report</Text>
         </View>
         <View style = {styles.ButtonContainer} >
           <RadioButton
@@ -80,67 +81,73 @@ export default function SuggestionsScreen() {
             status={ checked === 'Bug Report' ? 'checked' : 'unchecked' }
             onPress={() => setChecked('Bug Report')}
           />
-          <Text style = {{fontSize: 20,}}>Bug Report</Text>
-        </View>
+        <Text style = {{fontSize: 20,fontFamily: 'extrabold', color: '#202020',textShadowRadius: 5, textShadowColor: '#fff'}}>Bug Report</Text>
       </View>
-      <View style = {{margin: 5}} >
-        <Text style = {{fontSize: 18,}}>Fullname <Text style = {{fontStyle: 'italic'}}>(Optional)</Text></Text>
-        <View style = {styles.TextInput} >
-          <TextInput
-            style = {{marginLeft: 20, fontSize: 20}}
-            placeholder = 'e.g, John Doe'
-            onChangeText={(value) => setName(value)}
-            value={name}
-        />
-        </View>
+    </View>
+    <View style = {{margin: 5}} >
+      <Text style = {{fontSize: 17,fontFamily: 'extrabold', color: '#202020'}}>Fullname <Text style = {{fontStyle: 'italic'}}>(Optional)</Text></Text>
+      <View style = {styles.TextInput} >
+        <TextInput
+          onChangeText={(value) => setName(value)}
+          value={name}
+          style = {{marginLeft: 20, fontSize: 17, fontFamily: 'regular', width: '100%', height: '100%'}}
+          placeholder = 'e.g, John Doe'
+      />
       </View>
-      <View style = {{margin: 5}} >
-        <Text style = {{fontSize: 18, textAlign: 'left'}}>Subject</Text>
-        <View
-                
-                style = {styles.TextInput}> 
-                <Picker
-                        title = 'Select Category'
-                        selectedValue={value}
-                        mode="dropdown"
-                        style={{
-                            transform: [
-                               { scaleX: 1 }, 
-                               { scaleY: 1 },
-                            ],
-                        width: 600,
-                        bottom: 0,
-                        color: '#9e9e9e',
-                    
-                          }}
-                        onValueChange={(itemValue, itemIndex) => setvalue(itemValue)}
-                    >
-                        <Picker.Item label="Select" value="Select" />
-                        <Picker.Item label="Campus" value="CA" />
-                        <Picker.Item label="Building" value="BU" />
-                        <Picker.Item label="Faculty" value="FA" />
-                        <Picker.Item label="Colleges" value="CO" />
-                        <Picker.Item label="Organization" value="OR" />
-                        <Picker.Item label="Others" value="OT" />
-                    </Picker>
-                    
-        </View>
+    </View>
+    <View style = {{justifyContent: 'flex-start', margin: 5}}>
+    <Text style = {{fontSize: 17, textAlign: 'left',fontFamily: 'extrabold', color: '#202020'}}>Subject</Text>
+    
+    <View
+              
+                  style = {styles.TextInput}> 
+                  
+              <Picker
+                      title = 'Select Category'
+                      selectedValue={value}
+                      mode="dropdown"
+                      style={{
+                          transform: [
+                             { scaleX: 1 }, 
+                             { scaleY: 1 },
+                          ],
+                          width: '100%', 
+                          height: '100%',
+                      bottom: 0,
+                      color: '#000',
+                       fontFamily: 'regular'
+                        }}
+                      onValueChange={(itemValue, itemIndex) => setvalue(itemValue)}
+                  >
+                      <Picker.Item enabled = {false} label="Select" value="Select" />
+                      <Picker.Item label="Campus" value="CA" />
+                      <Picker.Item label="Building" value="BU" />
+                      <Picker.Item label="Faculty" value="FA" />
+                      <Picker.Item label="Colleges" value="CO" />
+                      <Picker.Item label="Organization" value="OR" />
+                      <Picker.Item label="Others" value="OT" />
+                  </Picker>
+                  
+                  </View>
       </View>
-       <View style = {{margin: 5}} >
-        <Text style = {{fontSize: 18,}}>Specify Complaint <Text style = {{fontStyle: 'italic'}}>(Optional)</Text></Text>
-        <View style = {styles.TextInput} >
-          <TextInput
-            style = {{marginLeft: 20, fontSize: 20}}
-            placeholder = 'e.g, John Doe or Office'
-            onChangeText={(value) => setSpecify(value)}
-            value={specify}
-        />
-        </View>
+
+     <View style = {{margin: 5}} >
+      <Text style = {{fontSize: 17,fontFamily: 'extrabold', color: '#202020',textShadowRadius: 5, textShadowColor: '#fff' }}>Specify Suggestion <Text style = {{fontStyle: 'italic'}}>(Optional)</Text></Text>
+      <View style = {styles.TextInput} >
+        <TextInput
+          style = {{marginLeft: 20, fontSize: 17, fontFamily: 'regular', width: '100%', height: '100%'}}
+          placeholder = 'e.g, John Doe or Office'
+          onChangeText={(value) => setSpecify(value)}
+          value={specify}
+      />
       </View>
-      <View style = {{margin: 20}} >
-        <Text  style = {{fontSize: 18,}}>Message</Text>
-        <View style = {styles.MessageInputtext} >
-          <TextInput
+    </View>
+  </View>
+  <View sytle = {{width: '50%', justifyContent: 'center', alignItems: 'center', alignContent: 'center'}}>
+  <View style = {{marginBottom: 20}} >
+      <Text style = {{fontSize: 17,fontFamily: 'extrabold', color: '#202020'}}>Message</Text>
+      <View style = {styles.MessageInputtext} >
+      <TextInput
             onChangeText={(value) => setText(value)}
             value={text}
             style = {{margin: 20, fontSize: 20}}
@@ -149,8 +156,8 @@ export default function SuggestionsScreen() {
             maxLength={1100}
         />
         </View>
-      </View>
-      <TouchableOpacity
+    </View>
+    <TouchableOpacity
          style = {styles.submit}
         onPress = {setNewReport}
         
@@ -160,7 +167,8 @@ export default function SuggestionsScreen() {
             </Text>
       </TouchableOpacity>
     </View>
-      <Text style = {{textAlign: 'center', position: 'absolute', top: 20, fontSize: 20, color: 'white',  }}>In case of emergency please call your local authority  </Text>
+      <Text style = {{textAlign: 'center', position: 'absolute', top: 20, fontSize: 20, color: '#202020',  }}>In case of emergency please call your local authority  </Text>
+    </View>
     </View>
   )
 };
@@ -184,6 +192,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#eb4034',
     borderRadius: 100,
     justifyContent: 'center',
+    alignSelf: 'center',
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -199,8 +208,8 @@ const styles = StyleSheet.create({
 
 
     backgroundColor: '#f2f3f7',
-    width: 600,
-    borderRadius: 15,
+    width: 400,
+    borderRadius: 5,
     height: 50,
     marginTop: 10,
     alignItems: 'center',
@@ -220,9 +229,9 @@ const styles = StyleSheet.create({
 
 
     backgroundColor: '#f2f3f7',
-    width: 600,
+    width: 500,
     borderRadius: 15,
-    height: 200,
+    height: 400,
     marginTop: 10,
     alignItems: 'flex-start',
     alignContent: 'flex-start',
@@ -240,12 +249,13 @@ const styles = StyleSheet.create({
 
   BoxContainer: {
 
-    width: '80%', 
+    width: '90%', 
     height: '85%', 
-    backgroundColor: '#fff', 
+    backgroundColor: '#f9f9f9',
     borderRadius: 20, 
     justifyContent: 'center', 
-    alignItems: 'center'
+    alignItems: 'center',
+    flexDirection: 'row'
 
 
   },
@@ -253,7 +263,7 @@ const styles = StyleSheet.create({
   ButtonContainer: {
     
     flexDirection: 'row', 
-    width: 100, 
+    width: 120, 
     height: 30, 
     margin: 25, 
     justifyContent: 'center', 
@@ -267,9 +277,8 @@ const styles = StyleSheet.create({
     borderRadius: 20, 
     width: 400
   
-  },
-
   
+}
 })
 
 
