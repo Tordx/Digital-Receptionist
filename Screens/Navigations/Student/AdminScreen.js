a6b3f472f5fe6aa9c27bb8caec44d745a73b953e
Roven Kyle Narra <ellamateogang@gmail.com>
2023-06-04 03:46:30 +0800

    admin separted categories

--- Screens/Navigations/Student/AdminScreen.js
+++ Screens/Navigations/Student/AdminScreen.js
@@ -47,8 +47,10 @@ import LinearGradient from 'react-native-linear-gradient';
     const [univVicePresidentQA, setUnivVicePresidentQA] = useState([]);
     const [showSearch, setShowSearch] = useState(false)
     const [scrollY, setScrollY] = useState(new Animated.Value(0));
-    const [modal, setModal] = useState(true)
+    const [modal, setModal] = useState(false)
     const [image, setImage] = useState('https://i.imgur.com/ruPofda.png');
+    const [selectedCategory, setSelectedCategory] = useState(null);
+
     useEffect(() => {
       memoizedGetAdminData()
 
@@ -106,7 +108,28 @@ import LinearGradient from 'react-native-linear-gradient';
       };
     }, [remoteDBAdmin, searchTerm]);
 
-    
+    const getDataByCategory = (category) => {
+      switch (category) {
+        case 'UP':
+          return univPresident;
+        case 'VP':
+          return univVicePresident;
+        case 'UCP':
+          return univCampusPresident;
+        case 'ALD':
+          return univVicePresidentAL;
+        case 'ASAD':
+          return univVicePresidentASAD;
+        case 'PFMD':
+          return univVicePresidentPFMD;
+        case 'REI':
+          return univVicePresidentREI;
+        case 'QA':
+          return univVicePresidentQA;
+        default:
+          return [];
+      }
+    };
 
     const RefreshList = () => {
 
@@ -117,29 +140,28 @@ import LinearGradient from 'react-native-linear-gradient';
     }
 
     const renderItem = ({ item }) => {
-
-      return(
-        <Pressable 
-        style = {styles.item}
-        android_ripple={{
-          color: 'blue',
-          borderRadius: 100,
-          radius: 200,
-        }} 
-        onPress={() => {
-           dispatch(setAdminData(item))
-        }} >
-          <ImageBackground style = {{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center',}} resizeMode='cover' source={require('../../../Assets/Img/background-lion.png')} >
-          <View style =  {{top: 5, position: 'absolute', justifyContent: 'center', alignItems: 'center',}} >
-            <Image resizeMode='cover' style = {{width: item.Image ?  230 : 150, height:item.Image ? 200 : 150, margin: 5, borderRadius: 5}} source = {{uri:  item.Image || image }}/>
-            <Text style = {styles.title}>{item.Name}</Text>
-            </View>
-            <Text style = {styles.office}>
-              {item.Office}
-            </Text>
-            </ImageBackground>
-      </Pressable>
-      )
+        return(
+          <Pressable 
+            style = {styles.item}
+            android_ripple={{
+              color: 'blue',
+              borderRadius: 100,
+              radius: 200,
+            }} 
+            onPress={() => {
+              dispatch(setAdminData(item))
+            }} >
+              <ImageBackground style = {{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center',}} resizeMode='cover' source={require('../../../Assets/Img/background-lion.png')} >
+              <View style =  {{top: 5, position: 'absolute', justifyContent: 'center', alignItems: 'center',}} >
+                <Image resizeMode='cover' style = {{width: item.Image ?  230 : 150, height:item.Image ? 200 : 150, margin: 5, borderRadius: 5}} source = {{uri:  item.Image || image }}/>
+                <Text style = {styles.title}>{item.Name}</Text>
+                </View>
+                <Text style = {styles.office}>
+                  {item.Office}
+                </Text>
+                </ImageBackground>
+          </Pressable>
+        )
     }
 
     return (
@@ -148,7 +170,7 @@ import LinearGradient from 'react-native-linear-gradient';
         style={styles.container}
         resizeMode = 'cover'
       >
-        <ScrollView>
+        <ScrollView style = {{width: '100%'}}>
           <View style = {styles.contentcontainer}>  
           {searchTerm ? (
             <View style = {{justifyContent: 'center', alignItems: 'center', alignSelf: 'center', flexDirection: 'column', flex: 1, marginTop: 20}}>
@@ -157,85 +179,48 @@ import LinearGradient from 'react-native-linear-gradient';
             onRefresh = {RefreshList}
             style = {{backgroundColor: 'green'}}
             />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, width: '100%', marginBottom: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: '#303030'}}>UNIVERSITY PRESIDENT</Text>
-            </View>
-          <FlatList
-            data={univPresident}
-            renderItem={renderItem}
-            keyExtractor={(item) => item._id}
-            
-          />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, margin: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: '#303030'}}>UNIVERSITY VICE PRESIDENTS</Text>
-            </View>
-            <FlatList
-              data={univVicePresident}
-              numColumns = {5}
-              renderItem={renderItem}
-              keyExtractor={(item) => item._id}
-              
-            />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, margin: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: '#303030'}}>OFFICE OF THE UNIVERSITY PRESIDENT</Text>
-            </View>
-            <FlatList
-              data={univCampusPresident}
-              numColumns = {5}
-              renderItem={renderItem}
-              keyExtractor={(item) => item._id}
-              
-            />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, margin: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: '#303030'}}>OFFICE OF THE VICE PRESIDENT FOR ADMINISTRATION AND LINKAGES</Text>
-            </View>
-            <FlatList
-              data={univVicePresidentAL}
-              numColumns = {5}
-              renderItem={renderItem}
-              keyExtractor={(item) => item._id}
-              
-            />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, margin: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: '#303030'}}>OFFICE OF THE VICE PRESIDENT FOR ACADEMIC AND STUDENT AFFAIRS</Text>
-            </View>
-            <FlatList
-              data={univVicePresidentASAD}
-              numColumns = {5}
-              renderItem={renderItem}
-              keyExtractor={(item) => item._id}
-              
-            />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, margin: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: '#303030'}}>OFFICE OF THE VICE PRESIDENT FOR PLANNING AND FINANCE MANAGEMENT</Text>
-            </View>
-            <FlatList
-              data={univVicePresidentPFMD}
-              numColumns = {5}
-              renderItem={renderItem}
-              keyExtractor={(item) => item._id}
-              
-            />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, margin: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: '#303030'}}>OFFICE OF THE VICE PRESIDENT FOR RESEARCH, EXTENSION AND INNOVATION</Text>
-            </View>
-            <FlatList
-              data={univVicePresidentREI}
-              numColumns = {5}
-              renderItem={renderItem}
-              keyExtractor={(item) => item._id}
-              
-            />
-            <View style = {{backgroundColor: '#fddf54', borderRadius: 5, padding: 10, margin: 20}}>
-            <Text style = {{fontSize: 20, fontWeight: 'bold', color: 'black'}}>OFFICE OF THE VICE PRESIDENT FOR QUALITY ASSURANCE</Text>
-            </View>
-            <FlatList
-              data={univVicePresidentQA}
-              numColumns = {5}
-              renderItem={renderItem}
-              keyExtractor={(item) => item._id}
-              
-            />
+            <Pressable
+              style={styles.listcontainer}
+              onPress={() => {setSelectedCategory('UP'); setModal(true)}}
+            >
+              <Text style={styles.listtext}>UNIVERSITY PRESIDENT</Text>
+            </Pressable>
+            <Pressable
+              style={styles.listcontainer}
+              onPress={() => {setSelectedCategory('VP'); setModal(true)}}
+            >
+              <Text style={styles.listtext}>UNIVERSITY VICE PRESIDENTS</Text>
+            </Pressable>
+            <Pressable
+              style={styles.listcontainer}
+              onPress={() => {setSelectedCategory('UCP'); setModal(true)}}
+            >
+              <Text style={styles.listtext}>OFFICE OF THE UNIVERSITY PRESIDENT</Text>
+            </Pressable>
+            <Pressable
+              style={styles.listcontainer}
+              onPress={() => {setSelectedCategory('ASAD'); setModal(true)}}
+            >
+              <Text style={styles.listtext}>VICE PRESIDENT FOR ACADEMIC SERVICES AND DEVELOPMENT</Text>
+            </Pressable>
+            <Pressable
+              style={styles.listcontainer}
+              onPress={() => {setSelectedCategory('PFMD'); setModal(true)}}
+            >
+              <Text style={styles.listtext}>VICE PRESIDENT FOR FINANCE AND MATERIALS DEVELOPMENT</Text>
+            </Pressable>
+            <Pressable
+              style={styles.listcontainer}
+              onPress={() => {setSelectedCategory('REI'); setModal(true)}}
+            >
+              <Text style={styles.listtext}>VICE PRESIDENT FOR RESEARCH, EXTENSION, AND INNOVATION</Text>
+            </Pressable>
+            <Pressable
+              style={styles.listcontainer}
+              onPress={() => {setSelectedCategory('QA'); setModal(true)}}
+            >
+              <Text style={styles.listtext}>VICE PRESIDENT FOR QUALITY ASSURANCE</Text>
+            </Pressable>
             </View>
         )
         : (
@@ -243,8 +228,8 @@ import LinearGradient from 'react-native-linear-gradient';
         )}
 
              </View> 
-</ScrollView>
-<LinearGradient colors = {['#f6f6f6', '#00000000','#00000000']} style = {{position: 'absolute', top: 0, justifyContent: 'center', alignItems: 'center', width: '100%', height: 250}}></LinearGradient>
+        </ScrollView>
+        <LinearGradient colors = {['#f6f6f6', '#00000000','#00000000']} style = {{position: 'absolute', top: 0, justifyContent: 'center', alignItems: 'center', width: '100%', height: 150}}></LinearGradient>
       <View style = {{backgroundColor: '#0f2ed6', padding: 10, borderRadius: 20, position: 'absolute', top: 20, elevation: 10, shadowColor: '#000'}}>
               {showSearch ? null : <Text style = {{fontSize: 30, fontWeight: 'bold', color: '#fff'}}>UNIVERSITY OFFICIALS</Text>}
                 </View>            
@@ -285,21 +270,48 @@ import LinearGradient from 'react-native-linear-gradient';
           />
           <Modal
           visible = {modal}
-          animationType='fade'
+          animationType='slide'
           onRequestClose={() => setModal(false)}
           statusBarTranslucent
+          transparent
           >
-            <Pressable style = {{width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center',}} onPressOut={() => setModal(false)} >
-              <Image style = {{width: '100%', height: '100%', }} resizeMode = 'cover' source={require('../../../Assets/Img/official_opening.png')} />
-              <Text style = {{position: 'absolute', bottom: 10, color: 'white'}} >Press anywhere to continue</Text>
-            </Pressable>
+            <View style = {{justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', backgroundColor: '#00000088'}}>
+              <FlatList
+                data={getDataByCategory(selectedCategory)}
+                renderItem={renderItem}
+                keyExtractor={item => item._id}
+                numColumns={4}
+                contentContainerStyle={{ justifyContent: 'center', alignItems: 'center' }}
+              />
+            </View>
           </Modal>    
       </ImageBackground>
     )
   
-    }
+  }
   
     const styles = StyleSheet.create({
+
+      listtext: {
+        
+        fontSize: 35, 
+        fontWeight: 'bold', 
+        color: '#303030',
+        textAlign: 'center'
+
+      
+      },
+
+      listcontainer: {
+        width: 1000,
+        height: 200,
+        backgroundColor: '#fddf54', 
+        borderRadius: 5, 
+        padding: 10, 
+        margin: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
   
       container: {
   
